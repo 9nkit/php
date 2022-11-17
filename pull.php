a2ec45a8514b6a1435739e6a194fca93df65ff6e
9NKIT <44654938+9nkit@users.noreply.github.com>
2022-11-18 00:06:47 +0530

    Update pull.php

--- pull.php
+++ pull.php
@@ -1,11 +1,8 @@
 <?php
 
-$old_path = getcwd();
 chdir('/var/www/test-pull');
-//make sure to make the shell file executeable first before running the shell_exec function
 $output = shell_exec('./git-pull.sh');
-chdir($old_path);
-
 echo $output;
+echo 'Deployment done';
 
 ?> 
